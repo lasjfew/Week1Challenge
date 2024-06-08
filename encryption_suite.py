950906ef803d458e8bc6154f69ac15d5aa9bd57a
Nathan Roberts <natedagrape2@gmail.com>
2024-06-08 10:52:50 -0400

    Updated ecdsa

--- encryption_suite.py
+++ encryption_suite.py
@@ -1,7 +1,7 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 import hashlib
 import hmac
-from ecdsa import SigningKey
+from ecdsa.keys import SigningKey, VerifyingKey
 import os
 
 
@@ -10,11 +10,16 @@ def apply_hmac(key: bytes, msg: bytes) -> bytes:
     return h.digest()
 
 
-def sign_ecdsa(msg: bytes) -> bytes:
-    sk = SigningKey.generate() # Generate SigningKey object
+def sign_ecdsa(key: bytes, msg: bytes) -> (bytes, bytes):
+    private_key = SigningKey.generate(hashfunc=hashlib.sha256()) # Generate SigningKey object
+    verifying_key = private_key.get_verifying_key()
 
-    # Can verify with vk = sk.verifying_key, vk.verify(signature, b'message')
-    return sk.sign(msg)
+    return private_key.sign(msg), verifying_key
+
+
+def verify_signature(msg: bytes, pub_key, signature) -> bool:
+    vk = VerifyingKey.from_string(bytes.fromhex(pub_key), hashfunc=hashlib.sha256)
+    return vk.verify(bytes.fromhex(signature), msg)
 
 
 def encrypt_symmetric(msg: bytes, key: bytes) -> (bytes, bytes):
